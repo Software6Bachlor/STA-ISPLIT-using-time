2caf8d1b2047e7269ea3f8ac52989cd1d4cbecfa
Mikkel Troelsen <mikkel.s.troelsem@gmail.com>
2026-03-31 14:45:16 +0200

    initial setup

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
+import subprocess
 from loader import loadData, retrieveModelNames, selectModels
 from parser import parseModel
+
+HOSTRESULTS = os.path.abspath("./results")
 
 def main():
     """Main entry point"""
@@ -11,6 +15,20 @@ def main():
     data = loadData(userInput)
     model = parseModel(data)
     print(model)
+
+def runDocker(memory: int):
+    """Run the builder with the given memory limit
+    Args:
+        memory (int): Memory limit in MB
+    """
+
+    subprocess.run([
+        "docker", "run",
+        "--rm",
+        "-m", f"{memory}m",
+        "-v", f"{HOSTRESULTS}:/results",
+        "simulation-image"
+    ])
 
 
 if __name__ == "__main__":
