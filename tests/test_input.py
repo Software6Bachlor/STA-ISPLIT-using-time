6556ed7e2ec83b8876ffa8ba65c6f58d7294fc54
Magnus Nielsen <magnusnielsen3107@hotmail.com>
2026-04-07 08:59:05 +0200

    fix test filepaths

--- tests/test_input.py
+++ tests/test_input.py
@@ -2,10 +2,10 @@ from loader import loadData
 from parser import parseModel
 
 files = [
-    "../../../03 - Ressources/Tools/Modest/Benchmarks/jani/long-sta.jani",
-    "../../../03 - Ressources/Tools/Modest/Benchmarks/jani/manufacturing-sta.jani",
-    "../../../03 - Ressources/Tools/Modest/Benchmarks/jani/tandem-queue.jani",
-    "../../../03 - Ressources/Tools/Modest/Benchmarks/jani/chain-sta.jani",
+    "models/benchmark/jani/long-sta.jani",
+    "models/benchmark/jani/chain-sta.jani",
+    "models/benchmark/jani/tandem-queue.jani",
+    "models/benchmark/jani/manufacturing-sta.jani",
 ]
 
 for path in files:
