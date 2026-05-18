ee1c0175bbf2e5a10eab603b34eefb4aea000f97
LasseOergaardAAU <lorgaa23@student.aau.dk>
2026-05-18 13:55:58 +0200

    setup

---
+++ benchmark_simulation.py
@@ -0,0 +1,16 @@
+from main import main
+
+def runSimulationBenchmark():
+    main()
+
+
+    ## Scaling benchmark.
+    ## run chain sta på N = {1,2,3,4,5,6,7,8,9,10} x:axis - n, y axis: estimated probability +- error margin.  fail_w 1, pass_w: 9, time-bound = 100, 
+    ## run long-sta with                            x-axis: y_threshold = 100 punkter mellem 0 og 20000, y axis: estimated probability +- error margin.
+    ## run manufacturing :
+
+
+if __name__ == "__main__":
+    runSimulationBenchmark()
+
+
